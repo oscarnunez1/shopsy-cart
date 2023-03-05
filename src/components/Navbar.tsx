c18c4a7fd6c9df18554121140dcb4b8d429eacfa
Oscar Nunez <oscar.nunezcaba@gmail.com>
2023-03-05 16:47:29 -0500

    create NavLink routes in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,16 @@
-import { Container, Navbar as BootstrapNav } from "react-bootstrap"
+import { Container, Nav, Navbar as BootstrapNav } from "react-bootstrap"
+import { NavLink } from "react-router-dom"
 
 export function Navbar() {
   return (
     <BootstrapNav className="bg-white shadow-sm mb-3">
-      <Container>Nav</Container>
+      <Container>
+        <Nav>
+          <Nav.Link to="/" as={NavLink}>Home</Nav.Link>
+          <Nav.Link to="/store" as={NavLink}>Store</Nav.Link>
+          <Nav.Link to="/about" as={NavLink}>About</Nav.Link>
+        </Nav>
+      </Container>
     </BootstrapNav>
   )
 }
