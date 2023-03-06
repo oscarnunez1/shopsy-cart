6ad19d83e14339da01369183f6f40d77bba1e2f2
Oscar Nunez <oscar.nunezcaba@gmail.com>
2023-03-06 16:11:36 -0500

    add body properties to card and styling

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -14,7 +14,14 @@ export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
         variant="top" 
         src={imgUrl} 
         height="200px" 
-        style={{ objectFit: "cover" }} />
+        style={{ objectFit: "cover" }} 
+      />
+      <Card.Body className="d-flex flex-column">
+        <Card.Title className="d-flex justify-content-space-between align-items-baseline mb-4">
+          <span className="fs-2">{name}</span>
+          <span className="ms-2 text-danger">{price}</span>
+        </Card.Title>
+      </Card.Body>
     </Card>
   )
 }
