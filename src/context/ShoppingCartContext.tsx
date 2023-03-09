68fb72ef4769aabd3f8b465b6a1e2a1619eb75d7
Oscar Nunez <oscar.nunezcaba@gmail.com>
2023-03-09 16:31:40 -0500

    create decreaseCartQuantity function

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -47,7 +47,23 @@ export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
     })
   }
 
+  function decreaseCartQuantity(id: number) {
+    setCartItems(currentItems => {
+      if (currentItems.find(item => item.id === id)?. quantity === 1) {
+        return currentItems.filter(item => item.id !== id)
+      } else {
+        return currentItems.map(item => {
+          if (item.id === id) {
+            return { ...item, quantity: item.quantity - 1}
+          } else {
+            return item
+          }
+        })
+      }
+    })
+  }
+
   return (
-    <ShoppingCartContext.Provider value={{ getItemQuantity, increaseCartQuantity }}>{children}</ShoppingCartContext.Provider>
+    <ShoppingCartContext.Provider value={{ getItemQuantity, increaseCartQuantity, decreaseCartQuantity }}>{children}</ShoppingCartContext.Provider>
   )
 }
