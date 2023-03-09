6a73e79480ff40396a98673dee479d736ec89f86
Oscar Nunez <oscar.nunezcaba@gmail.com>
2023-03-09 16:53:28 -0500

    make minor adjustment to StoreItem for missing curly and parenthesis

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -21,32 +21,46 @@ export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
 
   return (
     <Card className="h-100">
-      <Card.Img 
-        variant="top" 
-        src={imgUrl} 
-        height="200px" 
-        style={{ objectFit: "cover" }} 
+      <Card.Img
+        variant="top"
+        src={imgUrl}
+        height="200px"
+        style={{ objectFit: "cover" }}
       />
       <Card.Body className="d-flex flex-column">
-        <Card.Title className="d-flex justify-content-space-between align-items-baseline mb-4">
+        <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
           <span className="fs-2">{name}</span>
-          <span className="ms-2 text-danger">{formatCurrency(price)}</span>
+          <span className="ms-2 text-muted">{formatCurrency(price)}</span>
         </Card.Title>
         <div className="mt-auto">
           {quantity === 0 ? (
-            <Button className="w-100" onClick={() => increaseCartQuantity(id)}>+ Add To Cart</Button>
-          ) : <div className="d-flex align-items-center flex-column" style={{ gap: ".5rem"}}>
-            <div className="d-flex alig-items-center justify-content-center" style={{ gap: ".5rem" }}
+            <Button className="w-100" onClick={() => increaseCartQuantity(id)}>
+              + Add To Cart
+            </Button>
+          ) : (
+            <div
+              className="d-flex align-items-center flex-column"
+              style={{ gap: ".5rem" }}
             >
-              <Button onClick={() => decreaseCartQuantity(id)}>-</Button>
-              <div>
-                <span className="fs-3">{quantity}</span>
-                in cart
+              <div
+                className="d-flex align-items-center justify-content-center"
+                style={{ gap: ".5rem" }}
+              >
+                <Button onClick={() => decreaseCartQuantity(id)}>-</Button>
+                <div>
+                  <span className="fs-3">{quantity}</span> in cart
+                </div>
+                <Button onClick={() => increaseCartQuantity(id)}>+</Button>
               </div>
-              <Button onClick={() => increaseCartQuantity(id)}>+</Button>
+              <Button
+                onClick={() => removeFromCart(id)}
+                variant="danger"
+                size="sm"
+              >
+                Remove
+              </Button>
             </div>
-            <Button variant="danger" size="sm" onClick={() => increaseCartQuantity(id)}>Remove</Button>
-            </div>}
+          )}
         </div>
       </Card.Body>
     </Card>
